b53e19fd52beef8957ad41b707012e7c1958084c
satish1326 <120575664+satish1326@users.noreply.github.com>
2023-07-18 16:04:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,8 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 streamlit.header("Fruityvice Fruit Advice!")
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
+streamlit.header("Fruityvice Fruit Advice!")
+

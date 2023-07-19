14b75209bec02276507eaba25723d021f94336f9
satish1326 <120575664+satish1326@users.noreply.github.com>
2023-07-19 13:20:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,7 +71,12 @@ def get_fruit_load_list():
 if streamlit.button('Get fruit load list'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows=get_fruit_load_list()
-  streamlit.dataframe(my_data_rows)  
+  streamlit.dataframe(my_data_rows)
+
+def insert_row_snowflake(new_fruit):
+ with my_cnx_cursor() as my_cur:
+  my_cur.execute("insert into fruit_load_list values ('" +guava+ "')")
+  return "thanks for adding" +new_fruit
     
 
 

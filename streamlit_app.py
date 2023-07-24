2d3052349bc6edd9af4df281d05e7aa217d9d9ed
satish1326 <120575664+satish1326@users.noreply.github.com>
2023-07-24 20:01:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,18 @@ try:
    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
    streamlit.dataframe(fruityvice_normalized)
 
+streamlit.header("fruit load list contains")
+def get_fruit_load_list():
+ with my_cnx_cursor() as my_cur:
+  my_cur.execute("select *from fruit_load_list")
+  return my_cur.fetchall()
+
+if streamlit.button('Get fruit load list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows=get_fruit_load_list()
+  my_cnx.close()
+  streamlit.dataframe(my_data_rows)
+
 
 except URLError as e:
   streamlit.error()  

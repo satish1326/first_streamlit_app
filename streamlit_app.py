cbc7f1536e8ceb1fc8af37f94a83f9c45f8d4715
satish1326 <120575664+satish1326@users.noreply.github.com>
2023-07-24 19:57:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,3 +34,19 @@ def insert_row_snowflake(new_fruit):
   my_cur.execute("insert into fruit_load_list values ('" +guava+ "')")
   return "thanks for adding" +new_fruit
 
+streamlit.header("Fruityvice Fruit Advice!")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about')
+  if not fruit_choice:
+    streamlit.error("please select a fruit to get info")
+  else:
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+   streamlit.dataframe(fruityvice_normalized)
+
+
+except URLError as e:
+  streamlit.error()  
+ 
+streamlit.stop()
+
